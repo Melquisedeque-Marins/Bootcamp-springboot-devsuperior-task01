7f99a5755eabbde5743a8b6afffbbcbf79fa192a
melck <melck_junior@hotmail.com>
2022-08-05 10:13:08 +0100

    Project creation

---
+++ src/main/java/com/mck/task01/BootcampSpringbootTask01Application.java
@@ -0,0 +1,13 @@
+package com.mck.task01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringbootTask01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSpringbootTask01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mck/task01/BootcampSpringbootTask01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mck.task01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSpringbootTask01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
