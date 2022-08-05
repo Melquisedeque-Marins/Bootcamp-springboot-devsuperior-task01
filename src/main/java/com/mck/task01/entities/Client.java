9473c0b40b6a8724e4c629e9abef3d380ca6bee5
melck <melck_junior@hotmail.com>
2022-08-05 10:22:24 +0100

    Entity client

---
+++ src/main/java/com/mck/task01/entities/Client.java
@@ -0,0 +1,27 @@
+package com.mck.task01.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private Double income;
+    @Column(name = "birth_Date", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant birthDate;
+    private Integer children;
+
+}
